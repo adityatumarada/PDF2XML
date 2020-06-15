6d8d7cbb42df4bf67297cdc5e68bab2e34d113d6
adityatumrada <adityatumrada@gmail.com>
2020-06-16 02:28:57 +0530

    changed path for windows and linux systems

---
+++ src/main/java/com/example/pdf2xml/Constant.java
@@ -0,0 +1,16 @@
+package com.example.pdf2xml;
+
+public class Constant {
+    static final String PRIMARY = "primary";
+    static final String BOLD="bold";
+    static final String TEXT_ENTRY="text-entry";
+    static final String DONE="Done creating XML File";
+    static final String NON_TABULAR_DATA="non-tabular-data";
+    static final String TEXT="text";
+    static final String KEY="key";
+    static final String OPENPDF="OPEN PDF";
+    static final String OPENXMLFOLDER="OPEN XML FOLDER";
+    static final String XMLFILEEXTENSION=".xml";
+    static final String FXMLFILEEXTENSION=".fxml";
+    static final String PDFTOXMLCONVERSION="PDF to XML Converter";
+}
