a87b069a8eaf843165cd49c3ed2bb22081501943
Eshita Shukla <32108487+EshitaShukla@users.noreply.github.com>
2020-06-16 20:32:04 +0530

    Update PDFTableStripper.java

--- src/main/java/com/example/pdf2xml/PDFTableStripper.java
+++ src/main/java/com/example/pdf2xml/PDFTableStripper.java
@@ -23,6 +23,9 @@ import java.util.*;
  * Author : Eshita
  */
 
+/**Returns an Array od the Details objects Each element (Details object) contains a table with its coordinates and the page-number that it is present on
+*/
+
 public class PDFTableStripper extends PDFTextStripper
 {
 
