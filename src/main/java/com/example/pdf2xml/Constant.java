35fe6f76ecb9f798d2e801ab953a855927f533d1
Jui Pitale <46614816+JuiPitale@users.noreply.github.com>
2020-06-16 15:36:24 +0530

    Added javadoc comments in Constant

--- src/main/java/com/example/pdf2xml/Constant.java
+++ src/main/java/com/example/pdf2xml/Constant.java
@@ -1,5 +1,11 @@
 package com.example.pdf2xml;
 
+/**
+ *This class contains all String constants in use
+ *
+ * @author Jui Pitale
+ *
+ */
 public class Constant {
     static final String PRIMARY = "primary";
     static final String BOLD="bold";
@@ -13,4 +19,7 @@ public class Constant {
     static final String XMLFILEEXTENSION=".xml";
     static final String FXMLFILEEXTENSION=".fxml";
     static final String PDFTOXMLCONVERSION="PDF to XML Converter";
+    static final String CONVERTING="Converting...";
+    static final String ALERTWHILECONVERSION="Please don't close the main window, this will take a while!";
+    static final String COMPLETE="The extraction is complete";
 }
